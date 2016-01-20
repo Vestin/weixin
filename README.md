9b2421883b5926d346b69795cf94b7e1ba35c915
Shi vestin <vestinzds@gmail.com>
2016-01-20 10:01:53 +0000

    add methods (get user access token & get user info )

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # weixin
+
+## usage
+### config
+example
+
+```
+$config = [
+	'access_token_key_name'	=>'weixin_access_token',
+	'jsapi_ticket_key_name'	=>'weixin_jsapi_ticket',
+	'appid'		=>'wx1dcb74f95d60041f',
+	'secret'	=>'2ba6350b18c47cfc23e89b3dbcaed7ca',
+	'scope'		=>'snsapi_userinfo',
+];
+
+```
+
+### sign package
+
+```
+    $Weixin = new Weixin($config,$redis);
+    $signPackage = $Weixin->getSignPackage();
+```
+
+### getUserAccessToken
+//client side get the `code`
+`$user_access_token = $Weixin->getUserAccessToken($code)`
+
+### getUserInfo
+`$Weixin->getUserInfo($user_access_token,$openid,$user_refresh_token)`
--- src/Weixin.php
+++ src/Weixin.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @vestin 12-02
+ * @vestin 1-20
  * Weixin jiekou
  */
 namespace Weixin;
@@ -19,11 +19,8 @@ class Weixin {
 	 */
 	private $redis;
 
-	public function __construct($config = []){
+	public function __construct(array $config = [],$redis){
 		$this->config = $config;
-		$redis = new Redis();
-		$redis->connect('localhost','6379');
-		$redis->auth('');
 		$this->redis = $redis;
 	}
 
@@ -34,8 +31,7 @@ class Weixin {
 		if($token = $this->redis->get($this->config['access_token_key_name'])){
 			return $token;
 		}else{
-			//$response = $this->httpGet('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$this->config['appid'].'&secret='.$this->config['secret']);
-			$response = file_get_contents('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$this->config['appid'].'&secret='.$this->config['secret']);
+			$response = $this->httpGet('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$this->config['appid'].'&secret='.$this->config['secret']);
 
 			$res = json_decode($response,true);
 			if(isset($res['access_token']) && isset($res['expires_in'])){
@@ -58,8 +54,7 @@ class Weixin {
 			return $ticket;
 		}
 		$token = $this->getAccessToken();
-		//$response = $this->httpGet('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$token.'&type=jsapi');
-		$response = file_get_contents('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$token.'&type=jsapi');
+		$response = $this->httpGet('https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$token.'&type=jsapi');
 		$res = json_decode($response,true);
 		if($res['errcode']==0 && $res['errmsg']=='ok'){
 			$this->redis->setEx($this->config['jsapi_ticket_key_name'],$res['expires_in'],$res['ticket']);
@@ -109,6 +104,8 @@ class Weixin {
 	 * https get
 	 */
 	private function httpGet($url) {
+		return file_get_contents($url);
+
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_TIMEOUT, 500);
@@ -122,5 +119,90 @@ class Weixin {
 		curl_close($curl);
 
 		return $res;
+	}
+
+
+	/**
+	 * [getUserAccessToken description]
+	 * @Author   Vestin
+	 * @DataTime 2016-01-19T11:50:56+0800
+	 * @return 
+	 * 	"access_token":"ACCESS_TOKEN", 
+	 * 	"expires_in":7200, 
+	 * 	"refresh_token":"REFRESH_TOKEN", 
+	 * 	"openid":"OPENID", 
+	 * 	"scope":"SCOPE" 
+	 */
+	private function getUserAccessToken($code){
+		$response = $this->httpGet("https://api.weixin.qq.com/sns/oauth2/access_token?appid=".$this->config['appid']."&secret=".$this->config['secret']."&code=".$code."&grant_type=authorization_code");
+		if( $response && $res = json_decode($res) && !isset($res['errcode'])){
+			return $res;
+		}else{
+			return false;
+		}
+	}
+
+	/**
+	 * [getUserInfo description]
+	 * @Author   Vestin
+	 * @DataTime 2016-01-19T12:07:43+0800
+	 * @param    [type]                   $user_access_token  [description]
+	 * @param    [type]                   $openid             [description]
+	 * @param    [type]                   $user_refresh_token [description]
+	 * @return   [type]                                       [
+	 * openid	普通用户的标识，对当前开发者帐号唯一
+	 * nickname	普通用户昵称
+	 * sex	普通用户性别，1为男性，2为女性
+	 * province	普通用户个人资料填写的省份
+	 * city	普通用户个人资料填写的城市
+	 * country	国家，如中国为CN
+	 * headimgurl	用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像 * ，用户没有头像时该项为空
+	 * privilege	用户特权信息，json数组，如微信沃卡用户为（chinaunicom）
+	 * unionid	用户统一标识。针对一个微信开放平台帐号下的应用，同一用户的unionid是唯一的。
+	 * ]
+	 */
+	private function getUserInfo($user_access_token,$openid,$user_refresh_token){
+		$response = $this->httpGet("https://api.weixin.qq.com/sns/userinfo?access_token=".$user_access_token."&openid=".$openid);
+		if( $response && $res = json_decode($res) && !isset($res['errcode'])){
+			return $res;
+		}else{
+			return $this->getUserInfoByRefresh($openid,$user_refresh_token);
+		}
+	}
+
+	private function getUserInfoByRefresh($openid,$user_refresh_token){
+		$refresh_res = $this->refreshUserAcessToken($refresh_token);
+		if($refresh_res === false){
+			return false;
+		}else{
+			$response = $this->httpGet("https://api.weixin.qq.com/sns/userinfo?access_token=".$refresh_res['access_token']."&openid=".$openid);
+			if( $response && $res = json_decode($res) && !isset($res['errcode'])){
+				return $res;
+			}else{
+				return false;
+			}
+		}
+	}
+
+	/**
+	 * [refreshUserAcessToken description]
+	 * @Author   Vestin
+	 * @DataTime 2016-01-19T12:13:09+0800
+	 * @param    [type]                   $refresh_token [description]
+	 * @return   [type]                                  
+	 * "access_token":"ACCESS_TOKEN", 
+	 *  "expires_in":7200, 
+	 *  "refresh_token":"REFRESH_TOKEN", 
+	 *  "openid":"OPENID", 
+	 *  "scope":"SCOPE" 
+	 */
+	private function refreshUserAcessToken($refresh_token){
+		$response = $this->httpGet("https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=".$this->config['appid']."&grant_type=refresh_token&refresh_token=
+".$refresh_token);
+		if( $response && $res = json_decode($res) && !isset($res['errcode'])){
+			return $res;
+		}else{
+			return false;
+		}
 	}
 }
